0db750e2774fe2b0914ffc7a42b42a934e4768e4
Den <den.matvssnko.99@ukr.net>
2019-04-09 12:55:49 +0300

    work on the bugs

--- main.js
+++ main.js
@@ -1,72 +1,32 @@
 //slider
 
-const slider = document.getElementById("slider");
-const dots = document.getElementById("dots");
+$('.main-slider').slick({
+	dots: true,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1
+});
 
-function shiftClass(oldElem, newElem){
-	oldElem.classList.remove("active");
-	newElem.classList.add("active");
-}
+//to show popUpWindow "create Account"
 
-slider.addEventListener("click", (event) => {
+const userMenu = document.getElementById("user-menu");
 
-	function ButSwitchContains(string){
-		return elemButtonSwitch.classList.contains(string);
+userMenu.addEventListener("click", (event) => {
+	let elemLogin = event.target;
+
+	while(!elemLogin.classList.contains("user-login")) {
+		if(elemLogin === document.documentElement) break
+		elemLogin = elemLogin.parentNode;
 	}
+	if(!elemLogin.classList.contains("user-login")) return
 
-	let elemButtonSwitch = event.target;
-
-	while(  elemButtonSwitch !== slider &&
-			!ButSwitchContains("left") &&
-			!ButSwitchContains("right") &&
-			!ButSwitchContains("dot") ) {
-
-		elemButtonSwitch = elemButtonSwitch.parentNode;
-	}
-	if( elemButtonSwitch === slider &&
-		!ButSwitchContains("left") &&
-		!ButSwitchContains("right") &&
-		!ButSwitchContains("dot") ||
-		ButSwitchContains("active")) return
-
-	let elemActive = dots.getElementsByClassName('active')[0];
-
-	if(!elemActive && ButSwitchContains("dot")){
-		elemButtonSwitch.classList.add("active"); 
-		return
-	}
-
-	if(ButSwitchContains("left")){
-
-		if(!elemActive.previousSibling.previousSibling)return
-
-		shiftClass(elemActive, elemActive.previousSibling.previousSibling);
-	}else if(ButSwitchContains("right")){
-
-		if(!elemActive.nextSibling.nextSibling)return
-			
-		shiftClass(elemActive, elemActive.nextSibling.nextSibling);
-
-	}else if(ButSwitchContains("dot")){
-		shiftClass(elemActive, elemButtonSwitch);
-	}
-
-},false);
-
-//to show form "create Account"
-
-const createAccount = document.getElementById("createAccount");
-
-createAccount.addEventListener("click", (event) => {
-	if(event.target.tagName !== "A") return
-
-	let form = document.getElementsByClassName('popUpForm')[0];
-	form.style.display = "block";
+	let popUpWindow = document.getElementsByClassName('popUp-window')[0];
+	popUpWindow.classList.remove("popUp-window_hidden");
 
 	const delForm = document.getElementById("delForm");
-	delForm.addEventListener("click", w ,false);
-	function w (e){
-		form.style.display = "none";
-		delForm.addEventListener("click", w ,false);
+	delForm.addEventListener("click", eventDel ,false);
+	function eventDel (e){
+		popUpWindow.classList.add("popUp-window_hidden");
+		delForm.addEventListener("click", eventDel ,false);
 	}
 },false);
